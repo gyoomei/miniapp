62dda3acf461c84adefcd92fb9b42ab5b98358ba
root <root@localhost.localdomain>
2026-04-17 05:01:58 +0800

    Force game tab as initial render

--- src/app.js
+++ src/app.js
@@ -212,6 +212,6 @@ setCheckInStatus('Waiting for wallet connection');
 setGameStatus('Ready');
 resetGameState();
 setInterval(updateCountdown, 1000);
-setTab(1);
+requestAnimationFrame(() => setTab(1));
 els.message.textContent = 'Tap Start';
 els.message.classList.add('show');
